ef633085b6f9709f10d6dab73deaf05b80d1aaf9
eikyuu <v.duguet.dev@gmail.com>
2023-06-11 11:18:22 +0200

    fix path 2

--- ui/molecules/review/review.tsx
+++ ui/molecules/review/review.tsx
@@ -1,27 +1,26 @@
 import Image from 'next/image';
- import banner from '../../../public/images/banner.jpg';
-
- import BoldText from '../../atoms/textBold/textBold';
+import banner from '../../../public/images/banner.jpg';
+import BoldText from '../../atoms/textBold/textBold';
 import TextWithQuote from '../../atoms/textWithQuote/textWithQuote';
- interface ReviewProps {
-   name: string;
-   content: string;
- }
- function Review({ name, content }: ReviewProps): JSX.Element {
-   return (
-     <div className="flex items-center">
-       <Image
-         className="w-48 h-48 rounded-full object-cover border-8 border-white"
-         src={banner}
-         alt="Profile picture of {name}"
-         placeholder="blur"
-         quality={100}
-       />
-       <div className="pl-4">
-         <BoldText text={name} />
-         <TextWithQuote content={content} />
-       </div>
-     </div>
-   );
- }
- export default Review;
+interface ReviewProps {
+  name: string;
+  content: string;
+}
+function Review({ name, content }: ReviewProps): JSX.Element {
+  return (
+    <div className='flex items-center'>
+      <Image
+        className='w-48 h-48 rounded-full object-cover border-8 border-white'
+        src={banner}
+        alt='Profile picture of {name}'
+        placeholder='blur'
+        quality={100}
+      />
+      <div className='pl-4'>
+        <BoldText text={name} />
+        <TextWithQuote content={content} />
+      </div>
+    </div>
+  );
+}
+export default Review;
