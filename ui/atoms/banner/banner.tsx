0302d7a5a04a922ebee3a8858d9f9f604ec2f3c0
eikyuu <v.duguet.dev@gmail.com>
2023-06-16 22:11:38 +0200

    ajout swiper fix

--- ui/atoms/banner/banner.tsx
+++ ui/atoms/banner/banner.tsx
@@ -6,7 +6,7 @@ function Banner() {
   return (
     <Link href='/' prefetch={false}>
       <Image
-        className='w-full h-160 object-cover'
+        className='w-full h-1/2 object-cover md:h-96 lg:h-1/2 xl:h-1/2 2xl:h-1/2'
         src={banner}
         alt='banner'
         placeholder='blur'
--- ui/organisms/lastWalk/lastWalk.tsx
+++ ui/organisms/lastWalk/lastWalk.tsx
@@ -6,53 +6,57 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 // import required modules
-import { Pagination } from 'swiper';
+import { useEffect, useState } from 'react';
 function LastWalk({ dogWalk }: { dogWalk: any }): JSX.Element {
-
   //detect if the user is on mobile or not
-  const isMobile = window.innerWidth < 768;
+  const [isMobile, setIsMobile] = useState(false);
+
+  //detect resize of the window to change the isMobile state if needed
+
+  const handleResize = () => {
+    window.innerWidth < 768 ? setIsMobile(true) : setIsMobile(false);
+  };
+  
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
-
-    {isMobile ? (
-      
-      <section className='h-full w-11/12 mx-auto pt-10 pb-10'>
-        <Swiper
-          slidesPerView={'auto'}
-          spaceBetween={16}
-          className='w-full h-full'
-        >
-          {dogWalk.slice(0, 4).map((dogWalk: any) => (
-            <SwiperSlide key={dogWalk.id} className='!w-11/12 last:w-full'>
+      {isMobile ? (
+        <section className='h-full w-11/12 mx-auto pt-10 pb-10'>
+          <Swiper
+            slidesPerView={'auto'}
+            spaceBetween={16}
+            className='w-full h-full'
+          >
+            {dogWalk.slice(0, 4).map((dogWalk: any) => (
+              <SwiperSlide key={dogWalk.id} className='!w-11/12 last:w-full'>
+                <CardWalk
+                  city={dogWalk.city}
+                  description={dogWalk.description}
+                  slug={dogWalk.slug}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </section>
+      ) : (
+        <section className='h-full w-11/12 mx-auto flex flex-col justify-evenly pt-10 pb-10'>
+          <LargeTitle title='Les dernieres balade' />
+          <div className='container mx-auto flex flex-col flex-wrap justify-between pt-10 md:flex-row'>
+            {dogWalk.slice(0, 4).map((dogWalk: any) => (
               <CardWalk
+                key={dogWalk.id}
                 city={dogWalk.city}
                 description={dogWalk.description}
                 slug={dogWalk.slug}
               />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </section>
-    ) : (
-      <section className='h-full w-11/12 mx-auto flex flex-col justify-evenly pt-10 pb-10'>
-      <LargeTitle title='Les dernieres balade' />
-      <div className='container mx-auto flex flex-col flex-wrap justify-between pt-10 md:flex-row'>
-        {dogWalk.slice(0, 4).map((dogWalk: any) => (
-          <CardWalk
-            key={dogWalk.id}
-            city={dogWalk.city}
-            description={dogWalk.description}
-            slug={dogWalk.slug}
-          />
-        ))}
-      </div>
-    </section>
-    )
-    }
-
-
-
-
+            ))}
+          </div>
+        </section>
+      )}
     </>
   );
 }
