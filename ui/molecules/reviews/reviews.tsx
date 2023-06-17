5e55302f5d6d78098dbd3a80de6ba1c62563de94
eikyuu <v.duguet.dev@gmail.com>
2023-06-17 15:29:10 +0200

    text format

--- ui/molecules/reviews/reviews.tsx
+++ ui/molecules/reviews/reviews.tsx
@@ -5,7 +5,7 @@ function Reviews({reviews}: any): JSX.Element {
   return (
     <section className='h-full flex flex-col justify-evenly bg-primary pt-10 pb-10'>
       <MediumTitle title='Témoignages des utilisateurs' color='text-white' />
-      <div className='container mx-auto flex flex-col items-center justify-around pt-10 md:flex-row'>
+      <div className='container mx-auto w-11/12 flex flex-col items-center justify-around pt-10 md:flex-row'>
         {reviews.slice(0, 3).map((review: any) => (
           <div key={review.id} className='mb-10 last:mb-0 md:mb-0'>
             <Review  name={review.name} content={review.content} />
--- ui/organisms/presentation/presentation.tsx
+++ ui/organisms/presentation/presentation.tsx
@@ -3,28 +3,28 @@ import Card from '../../molecules/card/card';
 
 function Presentation() {
   return (
-    <section className='w-11/12 h-full mx-auto pt-10 pb-10 flex flex-col'>
+    <section className=' h-full w-11/12 mx-auto pt-10 pb-10 flex flex-col'>
       <LargeTitle title='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.' />
-      <div className='flex flex-col items-center justify-around pt-10 md:flex-row'>
+      <div className='flex flex-col items-center justify-around text-center pt-10 md:flex-row'>
         <div className="md:mr-10 last:mr-0 md:w-1/4">
           <Card
             title='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.'
             text='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin. Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin. Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.'
           />
         </div>
-        <div className="mr-10 last:mr-0 md:w-1/4">
+        <div className="md:mr-10 last:mr-0 md:w-1/4">
           <Card
             title='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.'
             text='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin. Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin. Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.'
           />
         </div>
-        <div className="mr-10 last:mr-0 md:w-1/4">
+        <div className="md:mr-10 last:mr-0 md:w-1/4">
           <Card
             title='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.'
             text='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin. Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin. Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.'
           />
         </div>
-        <div className="mr-10 last:mr-0 md:w-1/4">
+        <div className="md:mr-10 last:mr-0 md:w-1/4">
           <Card
             title='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.'
             text='Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin. Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin. Lorem Ipsum dolor sit amet, consecterur adpiscl ekt porin.'
