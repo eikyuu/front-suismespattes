c3e476512e582ae2485b3011da19f8717a54634a
eikyuu <v.duguet.dev@gmail.com>
2023-06-29 23:14:30 +0200

    test

--- next.config.js
+++ next.config.js
@@ -6,17 +6,23 @@ const nextConfig = {
       async headers() {
         return [
           {
-            // matching all API routes
-            source: "/api/:path*",
+            source: '/api/:path*',
             headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-          }
-        ]
-      }
+              { key: 'Access-Control-Allow-Credentials', value: 'true' },
+              { key: 'Access-Control-Allow-Origin', value: process.env.ALLOW_ORIGIN },
+              {
+                key: 'Access-Control-Allow-Methods',
+                value: 'GET, OPTIONS, PATCH, DELETE, POST, PUT',
+              },
+              {
+                key: 'Access-Control-Allow-Headers',
+                value:
+                  'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
+              },
+            ],
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
