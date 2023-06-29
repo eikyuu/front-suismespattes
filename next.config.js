e81bcd615e99ed2e0a8c98c8ec73dd99f1709e4b
eikyuu <v.duguet.dev@gmail.com>
2023-06-29 23:16:37 +0200

    refactoring fix

--- next.config.js
+++ next.config.js
@@ -1,28 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['au-fil-des-pattes.up.railway.app', 'localhost', 'flowbite.com', 'randomuser.me'],
-      },
-      async headers() {
-        return [
+  images: {
+    domains: [
+      'au-fil-des-pattes.up.railway.app',
+      'localhost',
+      'flowbite.com',
+      'randomuser.me',
+    ],
+  },
+  // next.config.js
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
-            source: '/api/:path*',
-            headers: [
-              { key: 'Access-Control-Allow-Credentials', value: 'true' },
-              { key: 'Access-Control-Allow-Origin', value: process.env.ALLOW_ORIGIN },
-              {
-                key: 'Access-Control-Allow-Methods',
-                value: 'GET, OPTIONS, PATCH, DELETE, POST, PUT',
-              },
-              {
-                key: 'Access-Control-Allow-Headers',
-                value:
-                  'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
-              },
-            ],
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
-        ];
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
-}
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
