7167f0fe858add23f6e57fac7c0d115e21f02fcc
eikyuu <v.duguet.dev@gmail.com>
2023-08-11 23:14:09 +0200

    test fix image

--- next.config.js
+++ next.config.js
@@ -1,34 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'au-fil-des-pattes.up.railway.app',
-      'au-fil-des-pattes-release.up.railway.app',
-      'au-fil-des-pattes-preprod.up.railway.app',
-      'localhost',
-      'flowbite.com',
-      'randomuser.me',
-      'aufildespattes-api.up.railway.app',
-      'aufildespattes-api-preprod.up.railway.app',
-    ],
+      remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'aufildespattes-api.up.railway.app',
+            pathname: '/walks/images/**',
+          },
+          {
+            protocol: 'http',
+            hostname: 'localhost:9000',
+            pathname: '/walks/images/**',
+          }
+      ],
+      domains: ['aufildespattes-api.up.railway.app', 'localhost'],
   },
-  // next.config.js
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://aufildespattes-api.up.railway.app/:path*',
-      },
-      {
-        source: '/api/:path*',
-        destination: 'https://aufildespattes-api-preprod.up.railway.app/:path*',
-      },
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/:path*',
-      }
-    ];
-  }
 };
 
 module.exports = nextConfig;
