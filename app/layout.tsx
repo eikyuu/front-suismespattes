863628362814da6d2ed5b1f76ebc2bcf18786c78
eikyuu <v.duguet.dev@gmail.com>
2023-08-20 17:33:15 +0200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from '../ui/molecules/footer/footer';
 import './globals.scss';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         </header>
         <Banner />
         {children}
+        <Analytics />
         <Footer />
         <Toaster />
       </body>
