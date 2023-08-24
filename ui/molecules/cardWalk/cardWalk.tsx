67c231b63675e1fbbfda065cbe93590b70cb2c3e
eikyuu <v.duguet.dev@gmail.com>
2023-08-24 21:58:21 +0200

    remove serch bar in meny

--- ui/molecules/cardWalk/cardWalk.tsx
+++ ui/molecules/cardWalk/cardWalk.tsx
@@ -25,20 +25,21 @@ function CardWalk({
       href={`balade/${slug}`}
       className='flex flex-col justify-between w-auto mb-4 xs:last:mb-0 md:w-72 lg:w-52 xl:w-72 focus:ring-4 focus:ring-tertiary focus:outline-none '
     >
-      <picture className='mb-4 max-w-xs'>    
+      <picture className='mb-4'>    
         <BlurImage 
         height='h-80'
-        alt={`Image de la balade ${city}`}
+        alt={`Image de la balade ${name} a ${city}`}
         image={`${process.env.NEXT_PUBLIC_API_URL}walks/images/${imageName}`} />
       </picture>
       <div className='mb-4'>
         <TextBold text={elipsis(name, 30)} color='text-black' />
+      </div>
+      <div className="mb-4">
         <TextBold text={elipsis(city, 30)} color='text-black' />
       </div>
       <div className='mb-4'>
         <TextGray text={elipsis(description, 100)} />
-      </div>
-      <Button text='Découvrir' />
+      </div>      <Button text='Découvrir' />
     </Link>
   );
 }
--- ui/molecules/contentNavigation/contentNavigation.tsx
+++ ui/molecules/contentNavigation/contentNavigation.tsx
@@ -47,7 +47,7 @@ function ContentNavigation() {
             </svg>
             <span className='sr-only'>Recherche</span>
           </button>
-          <div className='relative hidden md:block'>
+          {/* <div className='relative hidden md:block'>
             <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
               <svg
                 className='w-5 h-5 text-gray-500'
@@ -70,7 +70,7 @@ function ContentNavigation() {
               className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-4 focus:ring-tertiary focus:outline-none'
               placeholder='Trouve une balade par ville...'
             />
-          </div>
+          </div> */}
 
           <button
             onClick={handleMenuClick}
@@ -100,7 +100,7 @@ function ContentNavigation() {
           className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
           id='navbar-search'
         >
-          <div className='relative mt-3 md:hidden'>
+          {/* <div className='relative mt-3 md:hidden'>
             <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
               <svg
                 className='w-5 h-5 text-gray-500'
@@ -122,7 +122,7 @@ function ContentNavigation() {
               className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-4 focus:ring-tertiary focus:outline-none '
               placeholder='Trouve une balade par ville'
             />
-          </div>
+          </div> */}
           <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white'>
             <LiNav text='Accueil' href='/' />
             <LiNav text='Toutes les balades' href='/toutes-les-balades' />
