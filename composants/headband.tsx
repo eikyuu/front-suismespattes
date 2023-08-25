2ecf13b306afc540b278b732273d57b212f827de
vduguet <vduguet@apside.fr>
2023-08-25 11:31:01 +0200

    center

--- composants/headband.tsx
+++ composants/headband.tsx
@@ -1,7 +1,7 @@
 function Headband() {
   return (
     <div className='flex items-center justify-center p-1 bg-primary'>
-      <p className='text-white'>
+      <p className='text-white text-center'>
         Au fil des pattes est en cours de construction, n&apos;hésitez pas à
         nous contacter pour nous faire part de vos remarques.
         <a className='mx-1 font-semibold underline' href='/contact'>
