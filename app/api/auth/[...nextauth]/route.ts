49c029fa955e856cfebb9621ebfea65506ec5a29
eikyuu <v.duguet.dev@gmail.com>
2023-08-26 14:56:15 +0200

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,6 @@
 
 import NextAuth from "next-auth"
-import type { AuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import { authenticate } from '@/@core/services/authService'
-
-export const authOptions: AuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize (credentials, req) {
-        if (typeof credentials !== "undefined") {
-          const res = await authenticate(credentials.email, credentials.password)
-          if (typeof res !== "undefined") {
-            return { ...res.user, token: res.token }
-          } else {
-            return null
-          }
-        } else {
-          return null
-        }
-      }
-    })
-  ],
-  session: { strategy: "jwt" },
-  callbacks: {
-    jwt: async ({ token, user }) => {
-      if (user) {
-        token.user = user
-      }
-      return token
-    },
-    session: async ({ session, token }: any) => {
-      if (token) {
-        session.user = token.user
-        session.token = token.token
-      }
-      return session
-    }
-  }
-}
+import { authOptions } from './options'
 
 const handler = NextAuth(authOptions)
 
