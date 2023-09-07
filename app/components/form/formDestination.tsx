753482005b5d99fd073adbcbc917a3715a47cb7f
vduguet <vduguet@apside.fr>
2023-09-07 15:55:49 +0200

    fix commit

--- app/components/form/formDestination.tsx
+++ app/components/form/formDestination.tsx
@@ -221,7 +221,7 @@ function FormDestination({ slug }: { slug?: string }) {
           id='countries'
           className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit p-2.5'
         >
-          <option value='default'>Choissisez un pays</option>
+          <option value=''>Choissisez un pays</option>
           <option value='US'>United States</option>
           <option value='CA'>Canada</option>
           <option value='FR'>France</option>
