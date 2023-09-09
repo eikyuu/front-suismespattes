c23ae7ec83bebae548a18bc10c017ea3e8fb590a
eikyuu <v.duguet.dev@gmail.com>
2023-09-09 18:06:49 +0200

    liseret color

--- app/components/Headband.tsx
+++ app/components/Headband.tsx
@@ -1,7 +1,7 @@
 function Headband() {
   return (
-    <div className='flex items-center justify-center p-1 bg-primary'>
-      <p className='text-white text-center'>
+    <div className='flex items-center justify-center p-1 bg-white'>
+      <p className='text-gray-400 text-center'>
         Au fil des pattes est en cours de construction, n&apos;hésitez pas à
         nous contacter pour nous faire part de vos remarques.
         <a className='mx-1 font-semibold underline' href='/contact'>
