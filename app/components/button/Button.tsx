ac27ea3e6eeb4e3f7c72023f1a59cd4e2d1a39f7
eikyuu <v.duguet.dev@gmail.com>
2023-09-09 18:40:16 +0200

    fix btn

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 function Button({ text, className, onClick }: { text: string, className?: string, onClick?: () => void }): JSX.Element {
-  className = className ? className : "text-white bg-primary hover:bg-secondary ";
+  className = className ? className : "text-white bg-primary hover:bg-secondary";
   return (
     <button
-      className={`${className} text-w-max rounded-lg px-5 py-2.5 focus:ring-4 focus:ring-tertiary focus:outline-none`}
+      className={`${className} w-max rounded-lg px-5 py-2.5 focus:ring-4 focus:ring-tertiary focus:outline-none`}
       aria-label={text}
       onClick={onClick}
     >
