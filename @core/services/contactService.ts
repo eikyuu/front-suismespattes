53f9247a0180bd6d1a29c977ccc6a374483e4ffe
eikyuu <v.duguet.dev@gmail.com>
2023-10-29 10:26:25 +0100

    clear contact service add com

--- @core/services/contactService.ts
+++ @core/services/contactService.ts
@@ -1,6 +1,11 @@
 import { API_URL } from '../constants/global';
 
-export const postMessage = async (form: any) => {
+/**
+ * Sends a POST request to the API to post a message.
+ * @param form - The form data to be sent in the request.
+ * @returns A Promise that resolves to the response data from the API.
+ */
+export const postMessage = async (form: FormData): Promise<any> => {
     try {
         const response = await fetch(`${API_URL}contact`, {
             method: 'POST',
@@ -8,7 +13,7 @@ export const postMessage = async (form: any) => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(form),
-        })
+        });
         return await response.json();
     } catch (err) {
         console.error(err);
