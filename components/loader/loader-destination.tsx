96d043407b99f4d2d97ff5049e04207d8e59778f
eikyuu <v.duguet.dev@gmail.com>
2023-11-08 14:02:10 +0100

    loader destination

--- components/loader/loader-destination.tsx
+++ components/loader/loader-destination.tsx
@@ -1,53 +1,104 @@
+// import React from "react"
+
+// import { Skeleton } from "@/components/ui/skeleton"
+
+// function LoaderDestination() {
+//   return (
+//     <>
+//       <div className="container mt-5 flex w-full animate-pulse flex-col">
+//         <div className="w-full">
+//           <Skeleton className="h-[40px] w-[500px]" />
+//         </div>
+//         <div className="mt-3 flex w-full items-center space-x-3">
+//           <Skeleton className="h-[20px] w-[120px] rounded-lg" />
+//           <Skeleton className="h-[20px] w-[120px] rounded-lg" />
+//           <Skeleton className="h-[20px] w-[120px] rounded-lg" />
+//           <Skeleton className="h-[20px] w-[120px] rounded-lg" />
+//           <Skeleton className="h-[20px] w-[120px] rounded-lg" />
+//         </div>
+//         <div>
+//           <Skeleton className="mt-3 h-[20px] w-[200px] rounded-full" />
+//           <div className="mt-3 flex items-center space-x-3">
+//             <Skeleton className="h-[50px] w-[50px] rounded-full" />
+//             <Skeleton className="h-[20px] w-[150px]" />
+//           </div>
+//         </div>
+//         <div className="w-fullspace-x-3 mt-3 flex">
+//           <div className="w-full space-y-3 md:w-1/2">
+//             <Skeleton className="h-[20px] w-[600px]" />
+//             <Skeleton className="h-[20px] w-[500px]" />
+//             <Skeleton className="h-[20px] w-[600px]" />
+//             <Skeleton className="h-[20px] w-[500px]" />
+//             <Skeleton className="h-[20px] w-[600px]" />
+//             <Skeleton className="h-[20px] w-[500px]" />
+//           </div>
+//           <div className="w-full md:w-1/2">
+//             <div className="grid grid-cols-2 gap-3 md:grid-cols-4">
+//               <Skeleton className="h-[100px] w-[150px]" />
+//               <Skeleton className="h-[100px] w-[150px]" />
+//               <Skeleton className="h-[100px] w-[150px]" />
+//               <Skeleton className="h-[100px] w-[150px]" />
+//             </div>
+//             <Skeleton className="mt-3 h-[20px] w-[200px]" />
+//             <Skeleton className="mt-3 h-[394px] w-[644px]" />
+//           </div>
+//         </div>
+//         <Skeleton className="mt-3 h-[20px] w-[800px]" />
+//         <Skeleton className="mt-3 h-[20px] w-[600px]" />
+//       </div>
+//     </>
+//   )
+// }
+
+// export default LoaderDestination
+
 import React from "react"
+
+import { Skeleton } from "@/components/ui/skeleton"
 
 function LoaderDestination() {
   return (
     <>
-      <div className="container mt-5 flex w-full animate-pulse  space-x-3">
-        <div className="w-1/2">
-          <div className="flex-1 py-1">
-            <div className="space-y-3">
-              <div className="grid gap-4">
-                <div className="col-span-2 h-10 rounded bg-slate-300"></div>
-              </div>
-              <div className="grid grid-cols-3 gap-4">
-                <div className="col-span-2 h-2 rounded bg-slate-300"></div>
-                <div className="col-span-1 h-2 rounded bg-slate-300"></div>
-              </div>
-              <div className="h-2 rounded bg-slate-300"></div>
-              <div className="grid grid-cols-3 gap-4">
-                <div className="col-span-2 h-2 rounded bg-slate-300"></div>
-                <div className="col-span-1 h-2 rounded bg-slate-300"></div>
-              </div>
-              <div className="h-2 rounded bg-slate-300"></div>
-              <div className="h-2 rounded bg-slate-300"></div>
-              <div className="h-2 rounded bg-slate-300"></div>
-              <div className="grid gap-4">
-                <div className="col-span-2 rounded bg-slate-300"></div>
-              </div>
-              <div className="grid grid-cols-3 gap-4">
-                <div className="col-span-2 h-2 rounded bg-slate-300"></div>
-                <div className="col-span-1 h-2 rounded bg-slate-300"></div>
-              </div>
-              <div className="h-2 rounded bg-slate-300"></div>
-              <div className="grid grid-cols-3 gap-4">
-                <div className="col-span-2 h-2 rounded bg-slate-300"></div>
-                <div className="col-span-1 h-2 rounded bg-slate-300"></div>
-              </div>
-              <div className="h-2 rounded bg-slate-300"></div>
-              <div className="h-2 rounded bg-slate-300"></div>
-              <div className="h-2 rounded bg-slate-300"></div>
+      <div className="container mt-5 flex w-full animate-pulse flex-col">
+        <div className="w-full">
+          <Skeleton className="h-[40px] w-full md:w-[500px]" />
+        </div>
+        <div className="mt-3 flex w-full items-center space-x-3">
+          <Skeleton className="h-[20px] w-full rounded-lg md:w-[120px]" />
+          <Skeleton className="h-[20px] w-full rounded-lg md:w-[120px]" />
+          <Skeleton className="h-[20px] w-full rounded-lg md:w-[120px]" />
+          <Skeleton className="h-[20px] w-full rounded-lg md:w-[120px]" />
+          <Skeleton className="h-[20px] w-full rounded-lg md:w-[120px]" />
+        </div>
+        <div>
+          <Skeleton className="mt-3 h-[20px] w-full rounded-full md:w-[200px]" />
+          <div className="mt-3 flex items-center space-x-3">
+            <Skeleton className="h-[50px]  w-[50px] rounded-full" />
+            <Skeleton className="h-[20px] w-full md:w-[150px]" />
+          </div>
+        </div>
+        <div className="w-fullspace-x-3 mt-3 flex flex-col md:flex-row">
+          <div className="w-full space-y-3 md:w-1/2">
+            <Skeleton className="h-[20px] w-full md:w-[600px]" />
+            <Skeleton className="h-[20px] w-full md:w-[500px]" />
+            <Skeleton className="h-[20px] w-full md:w-[600px]" />
+            <Skeleton className="h-[20px] w-full md:w-[500px]" />
+            <Skeleton className="h-[20px] w-full md:w-[600px]" />
+            <Skeleton className="h-[20px] w-full md:w-[500px]" />
+          </div>
+          <div className="w-full md:w-1/2">
+            <div className="mt-3 grid grid-cols-2 gap-3 md:mt-0 md:grid-cols-4">
+              <Skeleton className="h-[100px] w-full md:w-[150px]" />
+              <Skeleton className="h-[100px] w-full md:w-[150px]" />
+              <Skeleton className="h-[100px] w-full md:w-[150px]" />
+              <Skeleton className="h-[100px] w-full md:w-[150px]" />
             </div>
+            <Skeleton className="mt-3 h-[20px] w-full md:w-[200px]" />
+            <Skeleton className="mt-3 h-[394px] w-full md:w-[644px]" />
           </div>
         </div>
-        <div className="w-1/2">
-          <div className="flex-1 space-y-6 py-1">
-            <div className="h-72 rounded bg-slate-300 md:h-96"></div>
-          </div>
-          <div className="flex-1 space-y-6 py-1">
-            <div className="h-72 rounded bg-slate-300 md:h-96"></div>
-          </div>
-        </div>
+        <Skeleton className="mt-3 h-[20px] w-full md:w-[800px]" />
+        <Skeleton className="mt-3 h-[20px] w-full md:w-[600px]" />
       </div>
     </>
   )
