815cf58c5e6ac0d15e3788f61be1a1890e704083
eikyuu <v.duguet.dev@gmail.com>
2023-12-01 23:26:27 +0100

    regex password

--- components/form/auth/form-register.tsx
+++ components/form/auth/form-register.tsx
@@ -25,7 +25,11 @@ import Text from "../../ui/text/Text"
 const formSchema = z.object({
   pseudo: z.string().min(3),
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z.string()
+  .min(8)
+  .refine((value) => /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value), {
+    message: 'Le mot de passe doit contenir au moins huit caractères avec des chiffres, des lettres et des caractères spéciaux.',
+  }),
 })
 
 function FormRegister() {
