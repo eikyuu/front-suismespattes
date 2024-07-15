c35a550fd907471de9224ce3845a1154ad485e36
vduguet <166373317+vduguet@users.noreply.github.com>
2024-07-15 16:37:21 +0200

    chore: Update banner image styling
    This commit updates the styling of the banner image in the `Banner` component. The `className` attribute is modified to improve the layout and responsiveness of the image. This change ensures that the banner image is displayed correctly across different screen sizes.

--- components/banner.tsx
+++ components/banner.tsx
@@ -6,7 +6,7 @@ function Banner() {
   return (
     <Link href="/" role="bannière">
       <Image
-        className="h-1/2 container object-cover md:h-96 lg:h-1/2 xl:h-160 lg:rounded-lg p-0 lg:mt-10"
+        className="container h-1/2 object-cover p-0 md:h-96 lg:mt-10 lg:h-1/2 lg:rounded-lg xl:h-160"
         src={banner}
         alt="Un chien debout sur un banc en bois, regardant au loin avec un arrière plan de montagnes et de forêts."
         loading="eager"
